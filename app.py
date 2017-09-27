aea9b1f8b2fcfd8d910636297e0d30da59effcad
Diego Maroto <diego.diemg@gmail.com>
2017-09-27 21:07:12 +0200

    Añadida persistencia en ficheros json. Ahora hay proyectos y dentro informes. Añadidos datos relevantes a la capa de persistencia. Añadido login falso al inicio del programa. Ctr+C te permite salir del proyecto para ir al menú superior.

--- app.py
+++ app.py
@@ -1,66 +1,153 @@
 #!/usr/bin/python3.5
+#json.loads : str to obj
+#json.dumps : obj to str
 import os
 import time
 import datetime
+import json
 clear = lambda: os.system('clear')
 try:
+    print("Login:")
+    usuario = input()
     while True:
         clear()
-        dir = "informes/"
-        informes = os.listdir(dir)
-        if len(informes) > 0:
-            print("Informes:")
+        print("Bienvenido "+usuario+"\n")
+        dir = "proyectos/"
+        proyectos = os.listdir(dir)
+        if len(proyectos) > 0:
+            print("Proyectos:")
             contador = 0
-            for f in informes:
+            for f in proyectos:
                 print(str(contador) +": " + f)
                 contador+=1
             print("")
-            print("Indica el id del informe que quieres abrir: ")
-        print("Introduce -1 para abrir un informe nuevo. [Ctr+C para salir]")
+            print("Indica el id del proyecto que quieres abrir: ")
+        print("Introduce -1 para abrir un proyecto nuevo. [Ctr+C para salir]")
         try:
             id = int(input())
         except ValueError:
-            print("Debes indicar el índice numérico de un informe o -1 para uno nuevo.\nInténtalo de nuevo.\n")
+            print("Debes indicar el índice numérico de un proyecto o -1 para uno nuevo.")
+            print("\n\nPresiona cualquier tecla para volver a empezar.")
+            input()
             continue
         clear()
         if id != -1 :
             try:
-                informe = informes[id]
-                print("Recuperando el informe...\n")
-                time.sleep(1)
-                print("Informe: "+informe+"\n")
-                with open(dir+informe,'r') as f:
-                    for line in f:
-                        print("· "+line)
-                
-                print("\n\n¿Quieres continuar el informe? [Y,n]")
-                continuar = input()
-                if not(continuar == "Y" or continuar == "YES" or continuar == "y" or continuar == "yes"):
-                    continue
+                proyecto = proyectos[id]
             except IndexError:
-                print("Ese informe no existe.\nInténtalo de nuevo.\n")
-                print("\n\nPresiona cualquier tecla para volver a empezar...")
+                print("Ese proyecto no existe.")
+                print("\n\nPresiona cualquier tecla para volver a empezar.")
                 input()
                 continue
+            try:
+                while True:
+                    clear()
+                    print("Recuperando el proyecto...\n")
+                    time.sleep(1)
+                    informes = os.listdir(dir+proyecto)
+                    if len(informes) > 0:
+                        print("Informes:")
+                        contador = 0
+                        for informe in informes:
+                            print(str(contador)+": "+informe)
+                            contador+=1
+                        print("")
+                        print("Indica el id del informe que quieres abrir: ")
+                    else:
+                        print("No hay informes.")
+                    print("Introduce -1 para abrir un informe nuevo. [Ctr+C para salir]")
+                    try:
+                        id = int(input())
+                    except ValueError:
+                        print("Debes indicar el índice numérico de un informe o -1 para uno nuevo.")
+                        print("\n\nPresiona cualquier tecla para volver a empezar.")
+                        input()
+                        continue
+                    if id != -1 :
+                        try:
+                            informe = informes[id]
+                        except IndexError:
+                            print("Ese informe no existe.")
+                            print("\n\nPresiona cualquier tecla para volver a empezar.")
+                            input()
+                            continue
+                        print("Informe: "+informe+"\n")
+                        f = open(dir+proyecto+"/"+informe,'r')
+                        line = f.read()
+                        try:
+                            obj = json.loads(line)
+                        except json.decoder.JSONDecodeError:
+                            print("El fichero está corrupto, no se puede leer el JSON.")
+                            print("\n\nPresiona cualquier tecla para volver a empezar.")
+                            input()
+                            continue
+                        print("Título: "+obj['titulo']+"\n")
+                        print("Gravedad: "+obj['urgencia']+"\n")
+                        for comentario in obj['comentarios']:
+                            print("· "+comentario)
+                        print("--------\nEstado: "+obj["estado"])
+                        print("\n\n¿Quieres continuar el informe? [Y,n,cerrar]")
+                        continuar = input()
+                        if continuar == "cerrar":
+                            obj['estado']='cerrado'  
+                        elif not(continuar == "Y" or continuar == "YES" or continuar == "y" or continuar == "yes"):
+                            continue
+                    else:
+                        now = datetime.datetime.now()
+                        informe = str(now.year)+str('{:02d}'.format(now.month))+ \
+                        str('{:02d}'.format(now.day))+ \
+                        str('{:02d}'.format(now.hour))+ \
+                        str('{:02d}'.format(now.minute))+".info"
+                        obj={}
+                        obj['usuario']=usuario
+                        obj['estado']='abierto'
+                        obj['comentarios']=[]
+                        print("Título de la incidencia:")
+                        obj['titulo'] = input()
+                        print("Elije el nivel de urgencia:\n\t0: poca, 1: media, 2: alta")
+                        try:
+                            urgencia=int(input())
+                            if urgencia>2 or urgencia<0:
+                                raise ValueError
+                            obj['urgencia']=str(urgencia)
+                        except ValueError:
+                            print("Debes elegir el identificador de una de las urgencias sugeridas.")
+                            print("\n\nPresiona cualquier tecla para volver a empezar.")
+                            input()
+                            continue
+                    print("Escribe tu comentario:")
+                    comentario = input()
+                    f = open(dir+proyecto+"/"+informe,'w')
+                    now = datetime.datetime.now()
+                    obj['comentarios'].append(usuario+" ["+\
+                        str(now.year)+str('{:02d}'.format(now.month))+"-"+ \
+                        str('{:02d}'.format(now.day))+" "+ \
+                        str('{:02d}'.format(now.hour))+":"+ \
+                        str('{:02d}'.format(now.minute))+"]: "+comentario)
+                    f.write(json.dumps(obj, sort_keys=True,indent=2, separators=(',', ':')))
+                    f.close()
+                    print()
+                    print("***************************************")
+                    print("*Actualización del informe almacenado.*")
+                    print("***************************************")
+                    print("\n\nPresiona cualquier tecla para volver a empezar.")
+                    input()
+            except KeyboardInterrupt:
+                continue
         else:
-            now = datetime.datetime.now()
-            informe = str(now.year)+str('{:02d}'.format(now.month))+ \
-            str('{:02d}'.format(now.day))+ \
-            str('{:02d}'.format(now.hour))+ \
-            str('{:02d}'.format(now.minute))+".info"
-
-        print("¿Quién eres?")
-        usuario = input()
-        print("Escribe tu comentario:")
-        comentario = input()
-        f = open(dir+informe,'a')
-        f.write(usuario+": "+comentario+"\n")
-        f.close()
+            print("Da nombre al proyecto:")
+            proyecto = input()
+            if os.path.exists(dir+proyecto):
+                print("El proyecto ya existe.")
+                print("\n\nPresiona cualquier tecla para volver a empezar.")
+                input()
+            else:
+                os.makedirs(dir+proyecto) 
         print()
         print("***************************************")
-        print("*Actualización del informe almacenado.*")
+        print("*Nuevo proyecto creado.*")
         print("***************************************")
-        print("\n\nPresiona cualquier tecla para volver a empezar...")
+        print("\n\nPresiona cualquier tecla para volver a empezar.")
         input()
 except KeyboardInterrupt:
-    print("\n\nBye  :)\n")
+    print("\n\nNos vemos :)\n")
